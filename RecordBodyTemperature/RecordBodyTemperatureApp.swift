76f4a0901f914156b7dd16fdcce4831003a8ba1d
Ryosukekamimura <come1234560@gmail.com>
2021-02-01 09:18:29 +0900

    Initial Commit

---
+++ RecordBodyTemperature/RecordBodyTemperatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordBodyTemperatureApp.swift
+//  RecordBodyTemperature
+//
+//  Created by 神村亮佑 on 2021/02/01.
+//
+
+import SwiftUI
+
+@main
+struct RecordBodyTemperatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
